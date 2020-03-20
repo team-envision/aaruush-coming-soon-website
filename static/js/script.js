fcec229cbacf9a4156785f6c96cb884c2bc06928
Ishan Chhabra <ishan.chhabra@hotmail.com>
2020-03-21 03:41:14 +0530

    Complete Part 4

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,55 @@
+/**
+ * Toggle stuff... 🕹
+ */
+
+var container = document.getElementById("container");
+var cursor = document.getElementById("cursor");
+var bsod = document.getElementById("bsod");
+var noiseScreen = document.getElementById("noise-screen");
+var aaruushUpgradingWrapper = document.getElementById(
+  "aaruush-upgrading-wrapper"
+);
+
+document.addEventListener("DOMContentLoaded", () => {
+  initPart1();
+});
+
+function initPart1() {
+  cursor.style.display = "block";
+
+  setTimeout(() => {
+    cursor.style.display = "none";
+    initPart2();
+  }, 2500);
+}
+
+function initPart2() {
+  bsod.style.display = "block";
+
+  setTimeout(() => {
+    bsod.style.display = "none";
+    initPart3();
+  }, 1500);
+}
+
+function initPart3() {
+  container.classList.toggle("white");
+  noiseScreen.style.display = "block";
+
+  setTimeout(() => {
+    noiseScreen.style.display = "none";
+    container.classList.toggle("white");
+    initPart4();
+  }, 1000);
+}
+
+function initPart4() {
+  aaruushUpgradingWrapper.classList.toggle("flex");
+}
+
+/**
+ * TV Grey Noise Screen 📺
+ */
 var canvas = document.getElementById("noise-screen"),
   ctx = canvas.getContext("2d");
 
